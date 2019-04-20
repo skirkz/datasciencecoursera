8e3614362bdddcdf489b83960caef6ab1a32e904
skirkz <48342006+skirkz@users.noreply.github.com>
2019-04-20 16:42:41 -0400

    Add files via upload

---
+++ cacheMatrix.R
@@ -0,0 +1,26 @@
+makeCacheMatrix <- function(x = matrix()) 
+{
+  invrs <- NULL
+  set <- function(y) 
+  {
+    x <<- y
+    invrs <<- NULL
+  }
+  get <- function() x
+  setinvrs <- function(inverse) invrs <<- inverse
+  getinvrs <- function() invrs
+  list(set = set, get = get, setinvrs = setinvrs, getinvrs = getinvrs)
+}
+
+cacheSolve <- function(x, ...) 
+{
+  invrs <- x$getinvrs()
+  if(!is.null(invrs)) {
+    message("getting cached data")
+    return(invrs)
+  }
+  data <- x$get()
+  invrs <- solve(data, ...)
+  x$setinvrs(invrs)
+  invrs
+}
